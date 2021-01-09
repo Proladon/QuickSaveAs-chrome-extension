6a2f03666a53d86365217222fd3540a3910ce27c
Proladon <proladon@gmail.com>
2021-01-10 00:11:46 +0800

    popup

---
+++ popup.js
@@ -0,0 +1,10 @@
+import {createApp} from 'vue'
+
+const addFolder_btn = document.getElementById("add-folder")
+
+
+const app = createApp({
+    // root instance definition
+  });
+  
+  app.mount("#app");
